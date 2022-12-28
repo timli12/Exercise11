170d9ed49ab09596269a2ca9e102b9880311b8db
timli12 <117094568+timli12@users.noreply.github.com>
2022-12-28 18:47:41 +0800

    update

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-//chatAPI.js
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-
-// TODO
-
-http.createServer(function (req, res) {
-    let path = url.parse(req.url, true).pathname;
-    console.log("Request:" + path);
-    if (path == "/") {
-        // TODO
-    } else if (path == "/chat") {
-        // TODO
-    } else if (path == "/save") {
-        // TODO
-    } else if (path == "/reload") {
-        // TODO
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const config = require("./config/config");
+const chatRouter = require("./routes/chat");
+const port = config.app.port;
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+mongoose.set('strictQuery', true);
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(config.db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+        });
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
     }
-    else {
-        res.end();
-    }
-}).listen(8080);
+  }
+  app.use("/", express.static('public', options));
+  app.use(express.urlencoded({ extended: true }));
+  //Routes go here
+  app.use("/",chatRouter);
+  //Connect to the database before listening
+  connectDB().then(() => {
+      app.listen(port, () => {
+          console.log("listening for requests");
+      })
+  })
--- routes/chat.js
+++ routes/chat.js
@@ -1,59 +1,52 @@
 const express = require("express");
-const Form = require("../models/form");
+const Chat = require("../models/chat");
 const router = express.Router();
-
-router.post("/app", async (req, res) => {
-    const form = new Form({
-        fullname: req.body.fullname,
-        identity: req.body.identity,
-        gender: req.body.gender,
-        phone: req.body.phone,
-        location: req.body.location,
-        date: req.body.date,
-        stime: req.body.stime,
-        etime: req.body.etime,
-        cook: req.body.cook,
-        bath: req.body.bath,
-        clean: req.body.clean
-    });
-    try{
-        const newForm = await form.save();
-        res.status(201).json("success");
-    } catch (err){
-        res.status(400).json({ message: err.message });
+router.use(express.urlencoded({ extended: true }));
+var chatRecords = [];
+router.get("/chat", async (req, res) => {
+    res.send(res.chat);
+});
+router.get("/chat/clear", async (req, res) => {
+    chatRecords.length = 0;
+});
+router.get("/chat/save", async (req, res) => {
+    for(let obj of chatRecords){
+        try{
+            await obj.save();
+        } catch (err){
+            res.status(400).json({ message: err.message });
+        }
     }
 });
-
-async function getId(req, res, next) {
-    let form;
+async function putChat(req, res, next) {
+    const chat = new Chat({
+        user: req.body.user,
+        say: req.body.say
+    });
+    chatRecords.push(chat);
+    for(let obj of chatRecords){
+        res.chat = chat;
+        next();
+    }
+}
+async function getChat(req, res, next) {
+    let chat;
     try {
-        form = await Form.findById(req.body.identity);
-        if (form == undefined) {
-            return res.status(404).json({ message: "Can't find todo" })
+        chat = await Chat.find({});
+        if (chat == undefined) {
+            return res.status(404).json({ message: "Can't find chat" })
         }
     } catch (err) {
         return res.status(500).json({ message: err.message })
     }
     // 如果有該事項 則將他加入到res中
-    res.form = form
+    res.chat = chat
+    chatRecords.push(chat);
     // 在router中執行middleware後需要使用next()才會繼續往下跑
     next();
 }
-
-router.post("/search", getId, async (req, res) => {
-    res.send(res.form)
-});
-
-router.post("/delete", getId, async (req, res) => {
-    try {
-        // 將取出的待辦事項刪除
-        await res.form.remove();
-        // 回傳訊息
-        res.json({ message: "Delete todo succeed" })
-    } catch (err) {
-        // 資料庫操作錯誤將回傳500及錯誤訊息
-        res.status(500).json({ message: "remove todo faild" })
-    }
+router.get("/chat/reload", getChat, async (req, res) => {
+    res.send(res.chat);
 });
 
 module.exports = router;
