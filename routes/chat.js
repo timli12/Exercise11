4a2888d1c0763d21515ad22737350a28170e4ede
timli12 <117094568+timli12@users.noreply.github.com>
2022-12-28 21:29:06 +0800

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -2,33 +2,6 @@ const express = require("express");
 const Chat = require("../models/chat");
 const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
-var chatRecords = [];
-router.get("/chat", putChat, async (req, res) => {
-    res.send(res.chat);
-});
-router.get("/chat/clear", async (req, res) => {
-    chatRecords.length = 0;
-});
-router.get("/chat/save", async (req, res) => {
-    for(let obj of chatRecords){
-        try{
-            await obj.save();
-        } catch (err){
-            res.status(400).json({ message: err.message });
-        }
-    }
-});
-async function putChat(req, res, next) {
-    const chat = new Chat({
-        user: req.body.user,
-        say: req.body.say
-    });
-    chatRecords.push(chat);
-    for(let obj of chatRecords){
-        res.chat = obj;
-        next();
-    }
-}
 async function getChat(req, res, next) {
     let chat;
     try {
@@ -41,10 +14,45 @@ async function getChat(req, res, next) {
     }
     // 如果有該事項 則將他加入到res中
     res.chat = chat
-    chatRecords.push(chat);
     // 在router中執行middleware後需要使用next()才會繼續往下跑
     next();
 }
+async function rmv(req, res, next) {
+    let chat;
+    try {
+        chat = await Chat.findById(req.body.id)
+        if (chat == undefined) {
+            return res.status(404).json({ message: "Can't find form" })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+    // 如果有該事項 則將他加入到res中
+    res.chat.remove()
+    // 在router中執行middleware後需要使用next()才會繼續往下跑
+    next();
+}
+router.get("/chat", async (req, res) => {
+    if(req.body.user != "" || req.body.say != ""){
+        const chat = new Chat({
+            user: req.body.user,
+            say: req.body.say
+        });
+        try{
+            await chat.save();
+        } catch (err){
+            res.status(400).json({ message: err.message });
+        }
+    }
+    getChat();
+    res.send(res.chat);
+});
+router.get("/chat/clear", rmv, async (req, res) => {
+    res.send("success");
+});
+router.get("/chat/save", async (req, res) => {
+    res.send("success");
+});
 router.get("/chat/reload", getChat, async (req, res) => {
     res.send(res.chat);
 });
