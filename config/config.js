7b8812cd8e0f33ebdccf4092500b7dbcda3d5ec6
timli12 <117094568+timli12@users.noreply.github.com>
2022-12-28 20:40:22 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
     app: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 8080
     },
     db:{
         url: "mongodb+srv://timli123:01057123htmltest@cluster0.6qoiglc.mongodb.net/?retryWrites=true&w=majority"
